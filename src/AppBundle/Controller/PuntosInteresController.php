244ddcc6ca9058da7166281cda254036cc466c1d
Roby Sottini <robysottini@gmail.com>
2016-04-29 21:01:08 -0300

    Twig.

---
+++ src/AppBundle/Controller/PuntosInteresController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PuntosInteresController extends Controller
+{
+	/**
+	 * @Route("/puntosinteres/index", name="puntosinteres_index")
+	 */
+	public function indexAction()
+	{
+		$puntosInteres = $this->getDoctrine()->getRepository('AppBundle:PuntoInteres')->findAll();
+
+		return $this->render('AppBundle:puntosinteres:index.html.twig', array('puntosinteres' => $puntosInteres));
+	}
+}
