41bf02cb52f07d28f0ab405d0426dac2ae14f81b
ariel <arielitu@gmail.com>
2016-05-06 21:28:48 -0300

    Clase de día 06/05/2016.

--- src/AppBundle/Controller/PuntosInteresController.php
+++ src/AppBundle/Controller/PuntosInteresController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace AppBundle\Controller;
-
+use AppBundle\Entity\PuntoInteres;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -13,8 +14,65 @@ class PuntosInteresController extends Controller
      */
     public function indexAction()
     {
+        
         $puntosInteres = $this->getDoctrine()->getRepository('AppBundle:PuntoInteres')->findAll();
 
         return $this->render('AppBundle:puntosinteres:index.html.twig', array('puntosinteres' => $puntosInteres));
+    }
+
+    /**
+     * @Route("/puntos-interes/filtro", name="puntosinteres_filtro")
+     */
+    public function filtroAction(Request $request)
+    {
+        $punto = new PuntoInteres();
+
+        $form = $this->createFormBuilder()
+            ->setMethod('GET')
+            ->add('nombre', 'text')
+            ->add('Filtrar', 'submit')
+            ->getForm();
+
+        $qb = $this->getDoctrine()->getManager()->createQueryBuilder('p');
+        $qb->select('p')
+            ->from('AppBundle:PuntoInteres','p');
+
+        $form->handleRequest($request);
+		
+        if($form->isValid()) {
+
+            $criteria = $form->getData();
+
+            $qb->where( $qb->expr()->like('p.nombre','?1'))
+                ->setParameter(1,'%' . $criteria['nombre'] . '%');
+        }    
+
+        $puntosInteres = $qb->getQuery()->getResult();
+        
+            /*$em = $this->getDoctrine()->getEntityManager();
+
+            $query = $em->createQuery(
+                'SELECT p 
+                FROM AppBundle:PuntoInteres p  
+                WHERE p.nombre like :nombre '
+            )->setParameter('nombre', '%'.$form->get('nombre')->getData().'%' );
+            $puntoAux = $query->getResult();            
+
+            return $this->render('AppBundle:puntosinteres:filtro.html.twig', array(
+                'puntoAux' => $puntoAux, 
+                'form' => $form->createView()
+            ));
+
+        }
+        else
+        {
+            echo 'NO! ENTRO!';
+        }
+
+
+    */
+        return $this->render(
+            'AppBundle:puntosinteres:filtro.html.twig', 
+            array('puntosInteres' => $puntosInteres, 'form' => $form->createView(),));
     }
 }
